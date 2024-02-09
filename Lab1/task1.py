91c159c23831bb5836ecd57636925a0dcd0113f8
VoGPro <krasnodar.kan@gmail.com>
2024-02-09 20:41:26 +0300

    Задание 1. Функция 3.

--- Lab1/task1.py
+++ Lab1/task1.py
@@ -1,3 +1,4 @@
+from math import sqrt
 # Задание 1. Вариант 6.
 # Функция 1. Найти сумму непростых делителей числа.
 
@@ -32,8 +33,60 @@ def sumOfNotPrimeDivs(num):
 # Функция 2. Найти количество цифр числа, меньших 3.
 def amountOf3(num):
     amount = 0
-    while (num != 0):
-        if (num % 10 < 3):
+    while num != 0:
+        if num % 10 < 3:
             amount += 1
         num //= 10
     return amount
+
+
+# Функция 3. Найти количество чисел,
+# не являющихся делителями исходного числа,
+# не взамнопростых с ним и взаимно простых с суммой простых цифр этого числа.
+
+def divisorsOf(num):
+    divs = []
+    for i in range(1, int(sqrt(num)) + 1):
+        if num % i == 0:
+            if num // i == i:
+                divs.append(i)
+            else:
+                divs.append(i)
+                divs.append(num // i)
+    return divs
+
+def sameElem(arr1, arr2):
+    for x in arr1:
+        for y in arr2:
+            if x == y and x != 1 and y != 1:
+                return True
+    return False
+
+# Функция 3.
+def findAmountOfNums(num):
+    sum = 0
+    tmp = num
+    while tmp != 0:
+        if isPrime(tmp % 10):
+            sum += tmp % 10
+        tmp //= 10
+    if sum == 0:
+        return 0
+    amount = 0
+    numDivs = divisorsOf(num)
+    for i in range(1, num):
+        if num % i != 0:
+            iDivs = divisorsOf(i)
+            sumDivs = divisorsOf(sum)
+            if sameElem(iDivs, numDivs) and not sameElem(sumDivs, iDivs):
+                amount += 1
+    return amount
+
+# Example:
+# 15
+# sum: 5
+# divs: 1 3 5 15
+# not divs: 2 4 6 7 8 9 10 11 12 13 14
+# not prime with num: 6 9 10 12
+# prime with sum: 6 9 12
+# amount: 3
