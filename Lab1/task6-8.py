bfcfe7707c2a8777cd1a65c77e74e0ffe2aba5fd
VoGPro <krasnodar.kan@gmail.com>
2024-02-13 14:40:12 +0300

    Задания 6-8. Задача 12.

--- Lab1/task6-8.py
+++ Lab1/task6-8.py
@@ -14,4 +14,25 @@ def moreThan5(s):
             amount += 1
     return amount
 
-print(moreThan5(s))
+# print(moreThan5(s))
+
+
+# 12. Дана строка. Необходимо найти те символы кириллицы,
+# которые не задействованы в данной строке.
+
+ruLetters = "глпщибпоыжактыяеоумршвалыфж"
+
+def findLetters(s):
+    alphabet = ['а', 'б', 'в', 'г', 'д', 'е', 'ё', 'ж', 'з', 'и', 'й', 'к', 'л', 'м', 'н', 'о',
+                'п', 'р', 'с', 'т', 'у', 'ф', 'х', 'ц', 'ч', 'ш', 'щ', 'ъ', 'ы', 'ь', 'э', 'ю', 'я']
+    length = len(alphabet)
+    i = 0
+    while i < length:
+        if s.count(alphabet[i]) > 0:
+            del alphabet[i]
+            i -= 1
+            length -= 1
+        i += 1
+    return alphabet
+
+print(findLetters(ruLetters))
