25fe59fc2ef3ca055da22c79d44315dbf4c564d0
VoGPro <krasnodar.kan@gmail.com>
2024-02-19 12:16:57 +0300

    Задания 15-19. Задача 18.

--- Lab1/task15-19.py
+++ Lab1/task15-19.py
@@ -12,11 +12,31 @@ def move3Left(arr):
     return arr
 
 a1 = [4, 2, 5]
-a2 = [6, 0, 9, 7]
+a2 = [6, 9, 0, 7]
 a3 = [1, 3, 8, 5, 0]
+# print(f"\n{a1}")
+# print(move3Left(a1))
+# print(f"\n{a2}")
+# print(move3Left(a2))
+# print(f"\n{a3}")
+# print(move3Left(a3))
+
+
+# 18. Дан целочисленный массив. Необходимо найти элементы,
+# расположенные перед первым минимальным.
+
+def findBeforeMin(arr):
+    beforeMin = []
+    minValue = min(arr)
+    i = 0
+    while arr[i] != minValue:
+        beforeMin.append(arr[i])
+        i += 1
+    return beforeMin
+
 print(f"\n{a1}")
-print(move3Left(a1))
+print(findBeforeMin(a1))
 print(f"\n{a2}")
-print(move3Left(a2))
+print(findBeforeMin(a2))
 print(f"\n{a3}")
-print(move3Left(a3))
+print(findBeforeMin(a3))
