81d8a4189d3b9da81f55472c4724979325a09c14
VoGPro <krasnodar.kan@gmail.com>
2024-02-19 13:03:13 +0300

    Задания 15-19. Задача 30.

--- Lab1/task15-19.py
+++ Lab1/task15-19.py
@@ -34,9 +34,38 @@ def findBeforeMin(arr):
         i += 1
     return beforeMin
 
-print(f"\n{a1}")
-print(findBeforeMin(a1))
-print(f"\n{a2}")
-print(findBeforeMin(a2))
-print(f"\n{a3}")
-print(findBeforeMin(a3))
+# print(f"\n{a1}")
+# print(findBeforeMin(a1))
+# print(f"\n{a2}")
+# print(findBeforeMin(a2))
+# print(f"\n{a3}")
+# print(findBeforeMin(a3))
+
+
+# 30. Дан целочисленный массив и натуральный индекс (число,
+# меньшее размера массива). Необходимо определить, является
+# ли элемент по указанному индексу локальным максимумом.
+
+def isLocalMaxima(index, arr):
+    n = len(arr)
+    mx = []
+
+    if (arr[0] > arr[1]):
+        mx.append(0)
+
+    for i in range(1, n - 1):
+        if (arr[i - 1] < arr[i] > arr[i + 1]):
+            mx.append(i)
+
+    if (arr[-1] > arr[-2]):
+        mx.append(n - 1)
+
+    return index in mx
+
+arr30 = [618, 107, 257, 324, 86, 432, 530]
+print(f"\n{arr30}")
+for i in range(len(arr30)):
+    if isLocalMaxima(i, arr30):
+        print(f"{arr30[i]} - локальный максимум")
+    else:
+        print(f"{arr30[i]} - не локальный максимум")
